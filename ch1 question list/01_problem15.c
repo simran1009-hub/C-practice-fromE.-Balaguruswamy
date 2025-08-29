b410cf583c16988501b9eae2a488dea82650558f
simran1009-hub <simranrani1009@gmail.com>
2025-08-30 00:20:23 +0530

    ch1 q15 solved

---
+++ ch1 question list/01_problem15.c
@@ -0,0 +1,9 @@
+//Q.Write a program to display the ASCII value of a character.
+#include<stdio.h>
+
+int main(){
+    char c='B';
+    printf("The character is %c\n",c);
+    printf("The ascii value of this character is %d\n",c);
+    return 0;
+}
