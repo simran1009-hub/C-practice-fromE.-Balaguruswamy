17be7bac7d437b944a28a3e47e86f723df97c4d1
simran1009-hub <simranrani1009@gmail.com>
2025-08-30 18:26:03 +0530

    ch2 q5 solved

---
+++ ch2 question list/02_problem5.c
@@ -0,0 +1,11 @@
+//Q. Write a program to demonstrate pre-increment and post-increment operators.
+#include<stdio.h>
+
+int main(){
+    int i=4;
+           printf("The value of i after using the pre-increment operator is %d\n",++i);
+           i=4;
+           printf("The value of i after using the post-increment operator is %d\n",i++);
+
+return 0;
+}
