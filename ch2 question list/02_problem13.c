ac16ff0af503895b781126e933e190328b562bbd
simran1009-hub <simranrani1009@gmail.com>
2025-08-30 21:47:21 +0530

    ch2 q13 solved

---
+++ ch2 question list/02_problem13.c
@@ -0,0 +1,8 @@
+//Q. Write a program to demonstrate operator precedence and associativity with examples.
+#include<stdio.h>
+
+int main(){
+float w=100,x=40,y=5,z=10;
+    printf("w*x/y+z = %.2f\n",w*x/y+z);
+    return 0;
+}
