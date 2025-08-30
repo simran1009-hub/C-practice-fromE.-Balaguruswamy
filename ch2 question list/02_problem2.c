9c4dc5da1b52440894ae41183ce718fa7f18eaec
simran1009-hub <simranrani1009@gmail.com>
2025-08-30 17:57:56 +0530

    ch2 q2 solved

---
+++ ch2 question list/02_problem2.c
@@ -0,0 +1,9 @@
+//Q. Write a program to add two numbers using variables of different data types.
+#include<stdio.h>
+
+int main(){
+    int i=4;
+    float f=7.9;
+    printf("The sum of i=%d and f=%f is %.2f\n",i,f,(i+f));
+    return 0;
+}
