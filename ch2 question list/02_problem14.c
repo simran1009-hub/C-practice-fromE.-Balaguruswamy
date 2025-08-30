d551d3078dfb29b8c38453d7cf0932c0017d657a
simran1009-hub <simranrani1009@gmail.com>
2025-08-30 21:50:33 +0530

    ch2 q14 solved

---
+++ ch2 question list/02_problem14.c
@@ -0,0 +1,9 @@
+//Q. Write a program to convert a given character to its ASCII value and vice versa.
+#include<stdio.h>
+
+int main(){
+    char c='A';
+    printf("The ascii value of the value in character variable c is %d\n",c);
+    printf("The charcater corresponding to the ascii value %d is %c\n",c,c);
+    return 0;
+}
