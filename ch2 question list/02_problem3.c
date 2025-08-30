4c821cc1b5a64851dfa408366fd022e44aa712d5
simran1009-hub <simranrani1009@gmail.com>
2025-08-30 18:05:44 +0530

    ch2 q3 solved

---
+++ ch2 question list/02_problem3.c
@@ -0,0 +1,13 @@
+//Q.Write a program to demonstrate integer, float, and char constants.
+#include<stdio.h>
+
+int main(){
+    int i=56;
+    float f=9.545;
+    char c='A';
+    
+    printf("integer variable i=%d\n",i);
+    printf("floating point variable f=%f\n",f);
+    printf("character variable c=%c\n",c);
+    return 0;
+}
