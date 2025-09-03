611921df4ec62961f916c7a395a9ccc3ca82f254
simran1009-hub <simranrani1009@gmail.com>
2025-09-03 20:19:19 +0530

    ch2 progrm_q1 solved

---
+++ ch2 question list/02_programming_exer_q1.c
@@ -0,0 +1,13 @@
+/*
+Write a program to display the equation of a line in the form
+ax + by = c
+for a=5, b=8 and c=18
+*/
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b = 8, c = 18;
+    printf("For a = 5,\nb = 8 and\nc=18\nThe equation of the line is %dx + %dy = %d\n", a, b, c);
+    return 0;
+}
