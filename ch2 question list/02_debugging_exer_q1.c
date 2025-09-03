3b3724efc94639285143a7ea8fb2d25a44f395cd
simran1009-hub <simranrani1009@gmail.com>
2025-09-03 18:04:12 +0530

    ch2 debugging_exer_q1 solved

---
+++ ch2 question list/02_debugging_exer_q1.c
@@ -0,0 +1,20 @@
+/*
+
+Find errors, if any, in the following program:
+
+/*A simple program
+int main()
+{
+//Does nothing
+}
+*/
+
+//ANSWER
+
+
+
+int main(){ 
+
+    return 0;//return statement must be present
+}
+//also while starting comment from "A simple program", the comment wasn't closed with "*/"
