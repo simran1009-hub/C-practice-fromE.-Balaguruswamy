69bccd5925b59fd92d8adf25a8298f84ab1e74cc
simran1009-hub <simranrani1009@gmail.com>
2025-09-18 07:29:06 +0530

    ch5 debugg_ex_1_d solved

---
+++ ch5 question list/05_debugging_exer_1_d.c
@@ -0,0 +1,13 @@
+/*
+State errors, if any, in the following input statements
+(d) scanf(\n"%f",root);
+*/
+#include<stdio.h>
+
+int main(){
+    float root;
+       scanf("%f",&root);         //We have to use "address of operator" i.e. "&" to read a character through scanf since we have to provide the address of the variable in order to store a variable in it.
+       
+                                
+    return 0;
+}
