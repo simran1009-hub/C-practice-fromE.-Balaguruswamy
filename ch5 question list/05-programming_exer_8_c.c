26d07cb1c6f3ff60a172a35d18c9941c4bb72179
simran1009-hub <simranrani1009@gmail.com>
2025-09-26 17:59:44 +0530

    ch5_prgrm_ex_8_c solved

---
+++ ch5 question list/05-programming_exer_8_c.c
@@ -0,0 +1,11 @@
+/*
+Write a program to print the value 345.6789 in fixed-point format with the following specifications:
+(c) correct to zero decimal places.
+*/
+#include<stdio.h>
+
+int main(){
+float n=345.6789;
+printf("%.0f\n",n);
+    return 0;
+}
