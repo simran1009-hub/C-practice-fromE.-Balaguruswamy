740be6471d8a624097a836eda7868f51d67e7bb3
simran1009-hub <simranrani1009@gmail.com>
2025-10-03 17:47:55 +0530

    ch6_programming_exer_4_g solved

---
+++ ch6 question list/06_programming_exer_4_g.c
@@ -0,0 +1,26 @@
+/*
+Given a list of marks ranging from 0 to 100, write a program to compute and print the number of students:
+(g) in the range 41 to 60, and
+The program should use a minimum number of if statements.
+*/
+#include<stdio.h>
+
+int main(){
+    int mark[7], count=0;
+    printf("This program counts the number of students who have obtained marks between 41 to 60\n. The marks entered should lie between 0 to 100 only\n");
+    for (int i = 0; i < 7; i++)
+    {
+        printf("Enter mark[%d]\n",i);
+        scanf("%d",&mark[i]);
+        if(mark[i]<0 || mark[i]>100){
+            printf("This mark is invalid. It can only lie in the range of 0 to 100\n");
+            return 1;
+        }
+       if(mark[i]>=41 && mark[i]<=60){
+            count++;
+        }
+    }
+    printf("%d number of students have scored in the range of 41 to 60\n",count);
+    
+    return 0;
+}
