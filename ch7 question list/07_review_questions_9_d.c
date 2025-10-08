eb5310f5dd5fc88ba6b67d3981ae19cb35cac7ad
simran1009-hub <simranrani1009@gmail.com>
2025-10-08 23:01:19 +0530

    ch7_review_questions_9_d solved

---
+++ ch7 question list/07_review_questions_9_d.c
@@ -0,0 +1,15 @@
+/*
+What would be the output of each of the following code segments?
+(d) for (m = 10; m > 7, m -=2)
+    printf(m);
+*/
+#include<stdio.h>
+
+int main(){
+    // for (int m = 10; m > 7, m -=2)
+    // {
+    //     printf("%d",m);
+    // }                                       // The output of the program is : 8642
+    
+    return 0;
+}
