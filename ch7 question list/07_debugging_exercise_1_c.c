1510706f262123d0851d338d49fcc646d5e34106
simran1009-hub <simranrani1009@gmail.com>
2025-10-10 23:31:48 +0530

    ch7_debugging_exercise_1_c solved

---
+++ ch7 question list/07_debugging_exercise_1_c.c
@@ -0,0 +1,16 @@
+/*
+Find errors, if any, in each of the following looping segments. Assume that all the variables have been declared 
+and assigned values.
+(c) do;
+    total = total + value;
+    scanf("%f", &value);
+    while (value != 999);
+*/
+#include<stdio.h>
+
+int main(){
+    // Error(s):
+    // 1. There should be curly braces after "do" and not semicolon(";"), meaning the two lines of block of code should be enclosed witin the curly braces.
+    
+        return 0;
+}
