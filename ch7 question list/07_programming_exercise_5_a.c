a645b34a72dccb35a50eb1ff2c0c5de14b1c1fea
simran1009-hub <simranrani1009@gmail.com>
2025-10-11 20:31:32 +0530

    ch7_programming_exercise_5_a solved

---
+++ ch7 question list/07_programming_exercise_5_a.c
@@ -0,0 +1,23 @@
+/*
+Write programs to print the following outputs using for loops.
+(a) 1
+    2 2
+    3 3 3 
+    4 4 4 4 
+    5 5 5 5 5
+*/
+#include<stdio.h>
+
+int main(){
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i ; j++)
+        {
+           printf("%d ",i);
+        }
+        printf("\n");
+        
+    }
+    
+    return 0;
+}
