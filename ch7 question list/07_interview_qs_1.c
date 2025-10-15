1630198c16a17e578b5d3bc99c876b870a4dab9a
simran1009-hub <simranrani1009@gmail.com>
2025-10-15 17:57:48 +0530

    ch7_interview_qs_1 solved

---
+++ ch7 question list/07_interview_qs_1.c
@@ -0,0 +1,17 @@
+/*
+1.What is the value of variable a after the following code is executed?
+    int a;
+    for(a=0;a<10;a++)
+    {
+    }
+
+2. What is the final value of x after the following code is executed,
+    for(x=9; x>=0; x--)
+    {
+    }
+*/
+
+//Answer 1: After the above given code is executed completely, the value of variable a will be equal to 10.
+
+//Answer 2: AFter the code given is executed entirely, the value of variable x will be equal to -1.
+ 
