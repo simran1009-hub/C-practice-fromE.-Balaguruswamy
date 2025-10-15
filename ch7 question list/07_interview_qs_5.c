a60d4f3908d8bc6b6d4d257b25007ced58f4ca9c
simran1009-hub <simranrani1009@gmail.com>
2025-10-15 19:24:30 +0530

    ch7_interview_qs_5 solved

---
+++ ch7 question list/07_interview_qs_5.c
@@ -0,0 +1,18 @@
+/*
+What will be the output of the foloowing code:
+        i=0;
+        while(i++<5)
+        {
+         if(i==0)
+          printf("Zero\n");
+         else
+          printf("Hello World\n");
+        }
+*/
+/*
+Answer: The output of this program is: Hello World
+                                       Hello World
+                                       Hello World
+                                       Hello World
+                                       Hello World
+*/
