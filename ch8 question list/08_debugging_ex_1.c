8bd08f745c8210ba5c2850374d083ec1a81d648f
simran1009-hub <simranrani1009@gmail.com>
2025-10-15 23:47:49 +0530

    ch8_debugging_ex_1_a solved

---
+++ ch8 question list/08_debugging_ex_1.c
@@ -0,0 +1,11 @@
+/*
+Identify errors, if any, in each of the following array declaration statments, assuming that ROW and 
+COLUMN are declared as symbolic constants:
+(a) int score (100);
+*/
+
+/*
+Error: The error is in the type of bracket used to declare the size of the array. The correct syntax
+        is: int score[100];
+        
+*/
