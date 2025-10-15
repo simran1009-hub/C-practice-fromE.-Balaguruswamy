aa26d02b528a7d803c2ef7785dd95c7845397877
simran1009-hub <simranrani1009@gmail.com>
2025-10-15 23:54:23 +0530

    ch8_debugging_ex_1_b solved

---
+++ ch8 question list/08_debugging_ex_1_b.c
@@ -0,0 +1,10 @@
+/*
+Identify errors, if any, in each of the following array declaration statments, assuming that ROW and 
+COLUMN are declared as symbolic constants:
+(b) float values [10,15];
+*/
+
+/*
+Error: In two-dimensional arrays, the the sizes are written in different brackets. Hence, the 
+        corrected syntax would be : float values[10][15];
+*/
