03b7ffab1dfd1a32130df8db342420f03df9f481
simran1009-hub <simranrani1009@gmail.com>
2025-10-16 00:19:58 +0530

    ch8_debugging_ex_1_d solved

---
+++ ch8 question list/08_debugging_ex_1_d.c
@@ -0,0 +1,10 @@
+/*
+Identify errors, if any, in each of the following array declaration statments, assuming that ROW and 
+COLUMN are declared as symbolic constants:
+(d) float average[ROW],[COLUMN];
+*/
+
+/*
+Error: The error is the comma between the sizes of this two-dimensional array.
+        Hence, the corrected syntax is: float average[ROW][COLUMN];
+*/
