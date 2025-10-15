133d563b421a687c97eec245d20f0cb50f165b85
simran1009-hub <simranrani1009@gmail.com>
2025-10-16 00:13:31 +0530

    ch8_debugging_ex_1_c solved

---
+++ ch8 question list/08_debugging_ex_1_c.c
@@ -0,0 +1,9 @@
+/*
+Identify errors, if any, in each of the following array declaration statments, assuming that ROW and 
+COLUMN are declared as symbolic constants:
+(c) char name[15];
+*/
+
+/*
+Error: There is no error in the above given syntax.
+*/
