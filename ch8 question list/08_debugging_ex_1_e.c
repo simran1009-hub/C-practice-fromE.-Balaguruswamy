040daa4e228dbde4d9dda2349eda9f35010665f4
simran1009-hub <simranrani1009@gmail.com>
2025-10-16 12:20:20 +0530

    ch8_debugging_ex_1_e solved

---
+++ ch8 question list/08_debugging_ex_1_e.c
@@ -0,0 +1,11 @@
+/*
+Identify errors, if any, in each of the following array declaration statments, assuming that ROW and 
+COLUMN are declared as symbolic constants:
+(e) double salary[ i + ROW]
+*/
+
+/*
+Error: Inside the bracket where we write the size of the array, one can only
+        write constants and not variables.
+        Hence, its corrected syntax will be: double salary[constant]
+*/
