b4db17d16d896867ea2047da0c0810ad5b5da6a5
simran1009-hub <simranrani1009@gmail.com>
2025-10-16 12:49:29 +0530

    ch8_debugging_ex_1_h solved

---
+++ ch8 question list/08_debugging_ex_1_h.c
@@ -0,0 +1,11 @@
+/*
+Identify errors, if any, in each of the following array declaration statments, assuming that ROW and 
+COLUMN are declared as symbolic constants:
+(h) int array x[COLUMN];
+*/
+
+/*
+Error: The error in this is the space left between 'array' and 'x'. In C, variable names cannot have
+        spaces in between.
+        Hence, the corrected code will be: int array_x[COLUMN];
+*/
