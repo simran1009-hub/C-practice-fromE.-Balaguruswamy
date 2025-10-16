bdfd68e3745ba4d7014f135f8564fa3d5928014a
simran1009-hub <simranrani1009@gmail.com>
2025-10-16 12:37:39 +0530

    ch8_debugging_ex_1_g solved

---
+++ ch8 question list/08_debugging_ex_1_g.c
@@ -0,0 +1,10 @@
+/*
+Identify errors, if any, in each of the following array declaration statments, assuming that ROW and 
+COLUMN are declared as symbolic constants:
+(g) int sum[];
+*/
+
+/*
+Error: Since we are not inititalizing the array in the same line of code where we are declaring it,
+        therfore, it is mandatory to mention the size of the array.
+*/
