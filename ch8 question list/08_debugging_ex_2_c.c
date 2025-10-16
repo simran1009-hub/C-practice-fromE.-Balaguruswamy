cda60b60d3071629b1bb1b6672da8fb74b316ac0
simran1009-hub <simranrani1009@gmail.com>
2025-10-16 23:25:26 +0530

    ch8_debugging_ex_2_c solved

---
+++ ch8 question list/08_debugging_ex_2_c.c
@@ -0,0 +1,8 @@
+/*
+char word[ ] = {'A', 'R', 'R', 'A', 'Y'};
+*/
+
+/*
+Errors: There is no error in this, since it is the declaration and initialization of a character
+        array and not a string.
+*/
