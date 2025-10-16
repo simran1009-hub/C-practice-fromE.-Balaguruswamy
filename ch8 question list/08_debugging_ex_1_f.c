e8912980c50f79ccf7751d8a21ee64d07a890ca9
simran1009-hub <simranrani1009@gmail.com>
2025-10-16 12:28:55 +0530

    ch8_debugging_ex_1_f solved

---
+++ ch8 question list/08_debugging_ex_1_f.c
@@ -0,0 +1,11 @@
+/*
+Identify errors, if any, in each of the following array declaration statments, assuming that ROW and 
+COLUMN are declared as symbolic constants:
+(f) long int number [ROW]
+*/
+
+/*
+Error: 1.Every intruction in C must be terminated with semi-colon
+        
+        Hence, corrected syntax is: long int number[ROW];
+*/
