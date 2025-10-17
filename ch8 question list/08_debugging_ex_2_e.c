c3944bb00eaffc7d02849dd167e57762b71f7d8d
simran1009-hub <simranrani1009@gmail.com>
2025-10-17 18:19:53 +0530

    ch8_debugging_ex_2_e solved

---
+++ ch8 question list/08_debugging_ex_2_e.c
@@ -0,0 +1,10 @@
+/*
+Identify errors, if any, in each of the following initialization statements.
+(e) float result[10] = 0;
+*/
+
+/*
+Errors: The correct way to inittialize all the elements of the array with the value '0' is: 
+        float result[10] = {0};
+        Therefore, the error was the abscence of curly braces.
+*/
