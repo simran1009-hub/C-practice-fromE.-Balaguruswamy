6732f34ef410449fe88e0c03a7271d6519c12415
simran1009-hub <simranrani1009@gmail.com>
2025-10-19 22:09:16 +0530

    ch8_debugging_5 solved

---
+++ ch8 question list/08_debugging_ex_5.c
@@ -0,0 +1,15 @@
+/*
+What is the output of the following program?
+main ( )
+{
+                    int m [ ] = { 1,2,3,4,5 } ;
+                    int x, y = 0;
+                    for (x = 0; x < 5; x++ )
+                                y = y + m [ x ];
+                    printf("%d", y) ;
+}
+*/
+
+/*
+Output of the above written C program is: 15
+*/
